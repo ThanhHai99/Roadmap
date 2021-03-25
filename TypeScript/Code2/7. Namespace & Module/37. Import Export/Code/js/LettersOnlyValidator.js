bfe6e3a67e7b64a68ed3f0ce321ce66a34c7d2a1
ThanhHai99 <haitran23031999@gmail.com>
2021-03-26 05:11:51 +0700

    Complete Import Export

---
+++ TypeScript/Code2/7. Namespace & Module/37. Import Export/Code/js/LettersOnlyValidator.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LettersOnlyValidator = void 0;
+const lettersRegexp = /^[A-Za-z]+$/;
+class LettersOnlyValidator {
+    isAcceptable(s) {
+        return lettersRegexp.test(s);
+    }
+}
+exports.LettersOnlyValidator = LettersOnlyValidator;
