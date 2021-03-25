f9ebad0bea93b716641d8ecae5888f97d7d15a8f
ThanhHai99 <haitran23031999@gmail.com>
2021-03-25 18:02:46 +0700

    Complete Generic Basic

---
+++ TypeScript/Code2/6. Generic/30. Generic Basic/Code/js/1.js
@@ -0,0 +1,12 @@
+function showNumberInfo(x) {
+    return x;
+}
+function showStringInfo(x) {
+    return x;
+}
+function showInfo(x) {
+    return x;
+}
+console.log(showInfo(true));
+console.log(showInfo(21));
+console.log(showInfo("11"));
