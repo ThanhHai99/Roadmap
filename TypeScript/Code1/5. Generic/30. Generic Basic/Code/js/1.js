743777a68311c4857d0373fa03475d57f80f6c76
ThanhHai99 <haitran23031999@gmail.com>
2021-06-26 11:25:28 +0700

    [26/06/2021]_TypeScript

--- TypeScript/Code1/5. Generic/30. Generic Basic/Code/js/1.js
+++ TypeScript/Code1/5. Generic/30. Generic Basic/Code/js/1.js
@@ -1,12 +1,16 @@
 function showNumberInfo(x) {
     return x;
 }
+;
 function showStringInfo(x) {
     return x;
 }
+;
+// Generic
 function showInfo(x) {
     return x;
 }
-console.log(showInfo(true));
-console.log(showInfo(21));
-console.log(showInfo("11"));
+;
+console.log(showInfo(true)); // true
+console.log(showInfo(21)); // 21
+console.log(showInfo("11")); // 11
