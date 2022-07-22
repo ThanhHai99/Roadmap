fa07a6f2c003a23c5dd979df59a376a92df0eebb
ThanhHai99 <hai.tran@kyanon.digital>
2022-07-22 09:55:07 +0700

    Fri 07/22/2022  9:55:07.68

---
+++ Go/Code1/controllers/productcontroller.go
@@ -0,0 +1,74 @@
+package controllers
+
+import (
+	"encoding/json"
+	"golang-crud-rest-api/database"
+	"golang-crud-rest-api/entities"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func CreateProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var product entities.Product
+	json.NewDecoder(r.Body).Decode(&product)
+	database.Instance.Create(&product)
+	json.NewEncoder(w).Encode(product)
+}
+
+func GetProductById(w http.ResponseWriter, r *http.Request) {
+	productId := mux.Vars(r)["id"]
+	if checkIfProductExists(productId) == false {
+		json.NewEncoder(w).Encode("Product Not Found!")
+		return
+	}
+	var product entities.Product
+	database.Instance.First(&product, productId)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(product)
+}
+
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+	var products []entities.Product
+	database.Instance.Find(&products)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(products)
+}
+
+func UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	productId := mux.Vars(r)["id"]
+	if checkIfProductExists(productId) == false {
+		json.NewEncoder(w).Encode("Product Not Found!")
+		return
+	}
+	var product entities.Product
+	database.Instance.First(&product, productId)
+	json.NewDecoder(r.Body).Decode(&product)
+	database.Instance.Save(&product)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(product)
+}
+
+func DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	productId := mux.Vars(r)["id"]
+	if checkIfProductExists(productId) == false {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode("Product Not Found!")
+		return
+	}
+	var product entities.Product
+	database.Instance.Delete(&product, productId)
+	json.NewEncoder(w).Encode("Product Deleted Successfully!")
+}
+
+func checkIfProductExists(productId string) bool {
+	var product entities.Product
+	database.Instance.First(&product, productId)
+	if product.ID == 0 {
+		return false
+	}
+	return true
+}
