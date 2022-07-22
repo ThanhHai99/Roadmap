b28435af4552a4bdea98eb5ba57194c5cb3c393e
ThanhHai99 <hai.tran@kyanon.digital>
2022-07-22 09:09:54 +0700

    2022/07/22 09:09

---
+++ Angular/_Code/src/app/Components/Techer/techer.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-techer',
+  templateUrl: './techer.component.html',
+  styleUrls: ['./techer.component.css']
+})
+export class TecherComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
